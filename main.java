50dfe745bc5cd69b064bb2faecdc3f202ca81c0c
lhlam0 <ryanlam0000@gmail.com>
2023-11-20 15:59:26 +0800

    Update main.java

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 // main.java
 
 import java.io.*;
-class main{
+class SalseSystem{
     static int main_menu(){
         System.out.print("Welcome to sales system!\n\n"+
                           "-----Main menu-----\n"+
@@ -12,10 +12,14 @@ class main{
                           "4. Exit this program\n"+
                           "Enter Your Choice: "); 
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        String input;
+        int input;
         try {
-            input = in.readLine();
-            return Integer.parseInt(input);
+            input = Integer.parseInt(in.readLine());
+            if (input == 1){
+                admin_menu();
+            }
+            return 0;
+
         } catch (IOException e) {
             System.err.println("Cannot read input");
             e.printStackTrace();
@@ -23,7 +27,34 @@ class main{
         return 0;
     }
     public static void main(String[] args){
-        int choice = main_menu();
-        System.out.println(choice);
+        int main_choice = main_menu();
+        // System.out.println(main_choice);
+        if (main_choice == 1){
+            int admin_choice = admin_menu(); 
+        }
+    }
+    private static int admin_menu() {
+        System.out.print("-----Operations for administrator menu----"+
+                         "What kinds of operation would you like to perform?"+
+                         "1. Create all tables"+
+                         "2. Delete all tables"+
+                         "3. Load from datafile"+
+                         "4. Show content of a table"+
+                         "5. Return to the main menu"+
+                         "Enter Your Choice: ");
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int input;
+        try {
+            input = Integer.parseInt(in.readLine());
+            if (input == 1){
+
+                
+            }
+            return 0;
+        } catch (IOException e) {
+            System.err.println("Cannot read input");
+            e.printStackTrace();
+        }  
+        return 0;
     }
 }
